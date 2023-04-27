8b5b7ccf28814661f1c75b991b9959c4e0030b02
Kundan Prajapati <114688530+kundanprajapati03@users.noreply.github.com>
2023-04-28 00:32:44 +0530

    Add files via upload

---
+++ assest/javascript/script.js
@@ -0,0 +1,12 @@
+//  home slider javascript 
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
